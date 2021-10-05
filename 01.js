430801983369bb35637c1dfba1cb36957da46766
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-05 23:44:36 +0300

    Add task11-12
    Increment a Number with JavaScript, Decrement a Number with JavaScript

--- 01.js
+++ 01.js
@@ -33,3 +33,10 @@ var difference = 45 - 33;
 var product = 8 * 10;
 var quotient = 66 / 33;
 
+//Increment a Number with JavaScript, Decrement a Number with JavaScript
+var myVar = 87;
+myVar ++;
+
+var myVar = 11;
+myVar--;
+
