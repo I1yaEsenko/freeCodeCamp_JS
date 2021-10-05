4d2b7d9bef576e3828cdcd37ea0feca7384d3719
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-05 23:17:13 +0300

    Add task5
    Understanding Uninitialized Variables

--- 01.js
+++ 01.js
@@ -1,2 +1,15 @@
 //Initializing Variables with the Assignment Operator
 var a = 9;
+
+//Understanding Uninitialized Variables
+var a;
+var b;
+var c;
+
+a = 5;
+b = 10;
+c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
