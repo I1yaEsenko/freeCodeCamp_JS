0deeebff30823106b9d2103a6d6f060eaa501726
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-29 20:29:24 +0300

    Replace Loops using Recursion

--- 01.js
+++ 01.js
@@ -891,6 +891,15 @@ do {
 console.log(myArray);
 
 // Replace Loops using Recursion
+function sum(arr, n) {
+  if (n <= 0){
+    return 0;
+  } else {
+    return sum(arr, n-1) + arr[n -1];
+  }
+}
+
+console.log(sum([2, 3, 4],3))
 
 // Profile Lookup
 
