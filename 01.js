06cb71462fa8eaca60e3e779188af364d329364f
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-29 21:53:53 +0300

    Use Recursion to Create a Countdown

--- 01.js
+++ 01.js
@@ -980,5 +980,15 @@ function checkSign(num) {
 
 console.log(checkSign(10));
 // Use Recursion to Create a Countdown
+function countdown(n){
+  if (n < 1){
+  return[];
+  } else {
+    const myArr = countdown (n - 1)
+      myArr.unshift(n)
+      return myArr;
+  }
+}
 
+console.log(countdown(5))
 // Use Recursion to Create a Range of Numbers
