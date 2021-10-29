c6a89bb7035ccad9cb825310eed6db2837f61dd5
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-29 21:15:03 +0300

    Generate Random Whole Numbers with JavaScript

--- 01.js
+++ 01.js
@@ -944,6 +944,10 @@ function randomFraction() {
 }
 
 // Generate Random Whole Numbers with JavaScript
+function randomWholeNum() {
+  return Math.floor(Math.random()*10);
+}
+console.log(randomWholeNum())
 
 // Generate Random Whole Numbers within a Range
 
