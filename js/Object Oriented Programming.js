e5dd58fbf7f7df0e6af28398c1435c295c8d8ea9
I1yaEsenko <ilya.esenko@gmail.com>
2021-10-31 12:45:20 +0300

    Iterate Over All Properties

--- js/Object Oriented Programming.js
+++ js/Object Oriented Programming.js
@@ -93,6 +93,24 @@ console.log(beagle.numLegs)
 
 
 // Task: Iterate Over All Properties
+// Итерация по всем свойства Объекта
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+let beagle = new Dog("Snoopy");
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in beagle){
+  if (beagle.hasOwnProperty(property)){
+    ownProps.push(property);
+  } else {
+    prototypeProps.push(property);
+  }
+}
+
 
 // Task: Understand the Constructor Property
 
