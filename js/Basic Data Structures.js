dedf2a440f81b561a11f2496bffe348a3d76069f
I1yaEsenko <ilya.esenko@gmail.com>
2021-11-10 00:02:57 +0300

    Copy Array Items Using slice()

--- js/Basic Data Structures.js
+++ js/Basic Data Structures.js
@@ -45,8 +45,12 @@ console.log(htmlColorNames(['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'Pal
 
 
 //Copy Array Items Using slice()
-//
-
+// Копирование части масслива при помощи Slice(), выбирает от какого элемента и до какого, не включая последний 
+function forecast(arr) {
+  let newArr = arr.slice(2,4)
+  return newArr;
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
 
 
 //Copy an Array with the Spread Operator
