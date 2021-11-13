51a2f50814e0e7e43bfe80adfa8c6643a600bfb1
I1yaEsenko <ilya.esenko@gmail.com>
2021-11-13 11:26:38 +0300

    Use getters and setters

--- js/ES6.js
+++ js/ES6.js
@@ -170,6 +170,22 @@ console.log(carrot.name); // Should display 'carrot'
 
 //Use getters and setters to Control Access to an Object
 //
+class Thermostat{
+  constructor(fahr){
+    this.fahr = fahr;
+  }
+  get temperature(){
+    return (5/9) * (this.fahr - 32);
+  }
+  set temperature(celsius){
+    this.fahr = ( celsius * 9.0)/ 5 + 32;
+  }
+}
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+
 
 //Create a Module Script
 //
